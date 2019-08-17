63e08b082d8d83110cf370893d78fca4a68a4c43
Chris <cnj.carmichael@gmail.com>
2019-08-17 11:48:09 -0400

    First commit. Client is sending "hello server" and server is responding with "hello client".

---
+++ NetworkingTest/Assets/Scripts/NetworkTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+
+public class NetworkTest : MonoBehaviour
+{
+    public bool isServer = false;
+    public int serverPort = 3000;
+    public int clientPort = 3001;
+
+    public IPAddress serverAddress = IPAddress.Loopback;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isServer)
+        {
+            UdpNetworking.initialize(serverPort);
+        }
+        else
+        {
+            UdpNetworking.initialize(clientPort);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (UdpNetworking.messageQueue.Count > 0)
+        {
+            Message msg = UdpNetworking.messageQueue.Dequeue();
+
+            print(msg.content);
+
+            if (isServer)
+            {
+                // respond to message
+                IPEndPoint endPoint = new IPEndPoint(msg.source.Address, clientPort);
+                UdpNetworking.send("Hello client!", endPoint);
+            }
+        }
+
+        if (!isServer && Input.GetKeyDown(KeyCode.Space))
+        {
+            UdpNetworking.send("Hello server!", new IPEndPoint(serverAddress, serverPort));
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        UdpNetworking.close();
+    }
+}
---
+++ NetworkingTest/Assets/Scripts/UdpNetworking.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using UnityEngine;
+
+using System.Collections.Generic;
+
+public struct Message
+{
+    public string content;
+    public IPEndPoint source;
+}
+
+public static class UdpNetworking
+{
+
+    public static Queue<Message> messageQueue = new Queue<Message>();
+
+    private static UdpClient sender, listener;
+    private static int _listenPort;
+
+    public static void initialize(int listenPort)
+    {
+        _listenPort = listenPort;
+        sender = new UdpClient();
+        listener = new UdpClient(_listenPort);
+
+        try
+        {
+            listener.BeginReceive(new AsyncCallback(receive), null);
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    private static void receive(IAsyncResult asyncResult)
+    {
+        IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, _listenPort);
+        byte[] received = listener.EndReceive(asyncResult, ref remoteIpEndPoint);
+
+        Message msg = new Message();
+        msg.content = Encoding.UTF8.GetString(received);
+        msg.source = remoteIpEndPoint;
+        messageQueue.Enqueue(msg);
+
+        listener.BeginReceive(new AsyncCallback(receive), null);
+    }
+
+    public static void send(string textToSend, IPEndPoint targetEndpoint)
+    {
+        if (textToSend.Length > 0)
+        {
+            byte[] sendBuffer = Encoding.ASCII.GetBytes(textToSend);
+
+            try
+            {
+                sender.BeginSend(sendBuffer, sendBuffer.Length, targetEndpoint, sendComplete, null);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Exception {e.Message}");
+            }
+        }
+    }
+
+    public static void sendComplete(IAsyncResult asyncResult)
+    {
+        UdpClient u = (UdpClient)asyncResult.AsyncState;
+
+        Console.WriteLine($"Send complete. Number of bytes sent: {u.EndSend(asyncResult)}");
+    }
+
+    public static void close()
+    {
+        sender.Close();
+        listener.Close();
+    }
+}
